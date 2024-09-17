cd7ada5eb84c824259b40bd4902e70e195b8d9cd
kaiser-lai <klai2027@kehillahstudent.org>
2024-09-17 09:37:18 -0700

    rest of the roomba work

--- roomba_level3.py
+++ roomba_level3.py
@@ -17,15 +17,44 @@ window = room.draw_room(level = 3, radius = 5)
 ###
 # Start your code here
 sq = 40
-forward(sq)
+
+forward(sq*5)
+def ski(bidi):
+    left(90)
+    forward(bidi)
+  
+
+ski(sq)
+ski(sq)
+ski(sq*2)
+ski(sq*2)
+ski(sq*3)
+ski(sq*3)
+ski(sq*4)
+ski(sq*4)
+ski(sq*5)
+ski(sq*5)
+ski(sq*6)
+ski(sq*6)
+ski(sq*7)
+ski(sq*6)
+ski(sq*8)
+ski(sq*6)
+ski(sq*7)
+ski(sq*7)
+ski(sq*6)
+ski(sq*8)
+ski(sq*6)
+ski(sq*4)
+ski(sq*3)
+
+forward(sq*5)
+backward(sq*10)
+forward(sq*5)
 right(90)
-forward(sq*3)
-
-
-
-
-
-
+forward(sq*5)
+backward(sq*10)
+forward(sq*5)
  
 # End your code here
 ###
--- roomba_level6.py
+++ roomba_level6.py
@@ -17,13 +17,68 @@ window = room.draw_room(level = 6, radius = 5)
 ###
 # Start your code here
 sq = 40
+left(180)
+forward(sq*3)
+right(90)
+def sqrfunc(spiral):
+        forward(spiral)
+        right(90)
 
-forward(sq)
+sqrfunc(sq*6)
+sqrfunc(sq*14)
+sqrfunc(sq*12)
+sqrfunc(sq*13)
+sqrfunc(sq*13)
+for i in range(2):
+      sqrfunc(sq*14)
+sqrfunc(sq*13)
+sqrfunc(sq*15)
+sqrfunc(sq*12)
+sqrfunc(sq*16)
+sqrfunc(sq*12)
+sqrfunc(sq*16)
+sqrfunc(sq*11)
+sqrfunc(sq*13)
+for i in range(2):
+       sqrfunc(sq*10)
+sqrfunc(sq*9)
+sqrfunc(sq*14)
+sqrfunc(sq*8)
+sqrfunc(sq*18)
+sqrfunc(sq*8)
+sqrfunc(sq*18)
+sqrfunc(sq*7)
+sqrfunc(sq*15)
+sqrfunc(sq*11)
+sqrfunc(sq*13)
+sqrfunc(sq*15)
+sqrfunc(sq*11)
+sqrfunc(sq*16)
+sqrfunc(sq*8)
+sqrfunc(sq*18)
+sqrfunc(sq*7)
+sqrfunc(sq*12)
+sqrfunc(sq*9)
+sqrfunc(sq*11)
+sqrfunc(sq*8)
+sqrfunc(sq*10)
+sqrfunc(sq*5)
+sqrfunc(sq*4)
+sqrfunc(sq*4)
+sqrfunc(sq*3)
+sqrfunc(sq*3)
+sqrfunc(sq*2)
+sqrfunc(sq*2)
+sqrfunc(sq)
 
+forward(sq*10)
+backward(sq*20)
+forward(sq*10)
+right(90)
+forward(sq*10)
+backward(sq*20)
+forward(sq*10)
 
-
-
- 
 # End your code here
 ###
  
